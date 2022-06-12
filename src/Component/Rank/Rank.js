feaaef944e6266fdb6e77cf6dac757a65b1a43ea
vbest001 <valmix21@gmail.com>
2022-06-12 23:32:55 +0200

    create components

---
+++ src/Component/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () =>{
+    return (
+      <div>
+          <div className='whie f3'>
+              {'Valmy, your current rank is...'}
+          </div>
+          <div className='white f1'>
+            {'#3'}
+          </div>
+      </div>
+    ); 
+}
+export default Rank; 
